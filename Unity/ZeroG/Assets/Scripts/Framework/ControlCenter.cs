b4eba0e46ea28ee8da86e5c353a74e82485724ed
cblythe <7Turandot!>
2015-08-15 10:58:13 -0400

    Adding the Main Control Center

---
+++ Unity/ZeroG/Assets/Scripts/Framework/ControlCenter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlCenter : Singleton<ControlCenter> {
+
+	
+	public void Awake() 
+    {
+        
+	}
+
+    public void Quit() 
+    {
+        Application.Quit();
+    }
+
+    public void LoadTeamSelect()
+    {
+        Application.LoadLevel("Team Select");
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadGame()
+    {
+        Application.LoadLevel("Game");
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScore()
+    {
+        Application.LoadLevel("Score");
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+}
