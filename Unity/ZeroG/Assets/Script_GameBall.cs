492c0ca426753066fcd22c71fd6cec80cd470c69
Kyle Morrand <kmorrand@302llc.com>
2015-08-15 17:35:44 -0400

    So much gameplay!!

---
+++ Unity/ZeroG/Assets/Script_GameBall.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script_GameBall : MonoBehaviour {
+
+
+	private int bounceCount;
+
+	private Rigidbody2D ballRigidBody;
+	private CircleCollider2D ballCollider;
+	private PhysicsMaterial2D bounceMat;
+
+	public int forceValue;
+	public Camera gameCamera;
+	public Vector3 target;
+
+
+	void Start () 
+	{
+		bounceCount = 0;
+
+		ballRigidBody = gameObject.GetComponent<Rigidbody2D>();
+		ballCollider = gameObject.GetComponent<CircleCollider2D>();
+		bounceMat = ballCollider.sharedMaterial;
+	}
+	
+
+	void Update () 
+	{	
+		//target = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+
+		//if(Input.GetMouseButtonDown(0))
+		//	ballRigidBody.AddForce(target * forceValue * Time.deltaTime, ForceMode2D.Impulse);
+	}
+
+	void OnCollisionEnter2D( Collision2D col)
+	{
+		if(col.collider.tag == "Environment")
+		{
+			bounceCount++;
+			
+			if(bounceCount == 2)
+				StopBounce();
+		}
+	}
+
+	public void pushBall(float pushX, float pushY){
+
+		Debug.Log ("Push the Ball!!");
+
+		ballRigidBody.isKinematic = false;
+		ballRigidBody.AddForce(new Vector2(pushX, pushY) * forceValue * Time.deltaTime, ForceMode2D.Impulse);
+		ballRigidBody.isKinematic = false;
+
+	}
+
+	void StopBounce()
+	{
+		ballRigidBody.velocity = Vector3.zero;
+		ballRigidBody.isKinematic = true;
+		bounceCount = 0;
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+
+		if (col.gameObject.tag == "Player") {
+			transform.parent = null;
+		}
+
+	}
+	
+}
